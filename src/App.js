53a4b46ff4232da191c700706a7dc6bb8a041759
khshakilahamed <khshakil.ahamed18@gmail.com>
2022-06-17 03:07:04 +0600

    connect with server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+const axios = require("axios");
+// eslint-disable-next-line import/first
+import { useState } from "react";
+// eslint-disable-next-line import/first
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  const addEmployee = () => {
+    console.log(name);
+    axios
+      .post("http://localhost:5000/create", {
+        name,
+        age,
+        country,
+        position,
+        wage,
+      })
+      .then(() => {
+        console.log("success");
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="information">
+        <label htmlFor="">Name:</label>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+        <label htmlFor="">Age:</label>
+        <input type="number" onChange={(e) => setAge(e.target.value)} />
+        <label htmlFor="">Country</label>
+        <input type="text" onChange={(e) => setCountry(e.target.value)} />
+        <label htmlFor="">Position:</label>
+        <input type="text" onChange={(e) => setPosition(e.target.value)} />
+        <label htmlFor="">Wage (year):</label>
+        <input type="number" onChange={(e) => setWage(e.target.value)} />
+        <button onClick={addEmployee}>Add Employee</button>
+      </div>
     </div>
   );
 }
